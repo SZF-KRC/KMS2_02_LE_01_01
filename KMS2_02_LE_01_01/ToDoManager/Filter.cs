95bcdbfc1ebf9e2ebd9c0d471d20621aa0842dbb
Jaroslav Krc <jaroslav.krc@gmail.com>
2024-06-17 11:40:21 +0200

    Projektdateien hinzufügen.

---
+++ KMS2_02_LE_01_01/ToDoManager/Filter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace KMS2_02_LE_01_01.ToDoManager
+{
+    public class Filter
+    {
+
+        /// <summary>
+        /// Delegate-Definition
+        /// </summary>
+        /// <param name="number">Die Ganzzahl, die gefiltert werden soll</param>
+        /// <returns>Ein boolescher Wert, der angibt, ob die Zahl die Bedingung erfüllt</returns>
+        public delegate bool FilterDelegate(int number);
+
+        /// <summary>
+        /// FilterList-Methode, die die Liste nach Delegaten filtert
+        /// </summary>
+        /// <param name="numbers">Die Liste der Ganzzahlen</param>
+        /// <param name="filter">Der Delegat, der die Filterbedingung definiert</param>
+        /// <returns>Eine neue Liste, die nur die Elemente enthält, für die der Delegat true zurückgibt</returns>
+        public static List<int> FilterList(List<int> numbers, FilterDelegate filter)
+        {
+            List<int> filteredList = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (filter(number))
+                {
+                    filteredList.Add(number);
+                }
+            }
+            return filteredList;
+        }
+    }
+}
---
+++ KMS2_02_LE_01_01/ToDoManager/ToDo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMS2_02_LE_01_01.ToDoManager
+{
+    public class ToDo
+    {
+        /// <summary>
+        /// Führt die Hauptlogik des Programms aus.
+        /// </summary>
+        public void Run()
+        {
+            // Erstelle eine Liste mit Ganzzahlen
+            List<int> numbers = new List<int> { -20, -10, -2, 0, 5, 10, 15, 20, -33, -46, 46, -4, 99 };
+
+            // Definieren eines Delegaten, der für positive Zahlen „true“ zurückgibt
+            Filter.FilterDelegate isPositive = number => number > 0;
+
+            // Verwendung der FilterList-Methode aus der Filter-Klasse
+            List<int> positiveNumbers = Filter.FilterList(numbers, isPositive);
+
+            // Die gefilterte Liste ausgeben
+            Console.WriteLine("Positive numbers:");
+            foreach (int number in positiveNumbers) { Console.WriteLine(number); }
+
+            Console.ReadKey();
+        }
+    }
+}
